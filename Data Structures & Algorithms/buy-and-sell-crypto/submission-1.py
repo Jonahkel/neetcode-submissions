a805fef6160462de8bc9095b4270203a0e793ad0
Jonah Kelman <84198590+Jonahkel@users.noreply.github.com>
2026-07-26 11:47:17 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_left = [0] * len(prices)
+        max_right = [0] * len(prices)
+        for idx, price in enumerate(prices):
+            if idx == 0:
+                min_left[idx] = prices[idx]
+            else:
+                min_left[idx] = min(min_left[idx-1], prices[idx])
+        for idx, price in enumerate(reversed(prices)):
+            og_idx = len(prices) - 1 - idx
+            if og_idx == len(prices) - 1:
+                max_right[og_idx] = prices[og_idx]
+            else:
+                max_right[og_idx] = max(prices[og_idx], max_right[og_idx+1])
+    
+        return max(right - left for left, right in zip(min_left, max_right))                
