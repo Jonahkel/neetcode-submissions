aa552281998cae8a5b0b1953a11d1a576c67984e
Jonah Kelman <84198590+Jonahkel@users.noreply.github.com>
2026-08-07 15:18:06 -0400

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.py
@@ -0,0 +1,12 @@
+from functools import cache
+class Solution:
+    
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def helper(m,n):
+            if m == 1 or n == 1: return 1
+            if m > n:
+                m, n = n,m
+            return helper(m-1,n) + helper(m,n-1)
+        
+        return helper(m,n)
